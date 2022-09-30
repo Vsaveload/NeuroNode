204dd5110a05593b7c535cfea1ad3d5ddce6a48b
VSaveLoad <vs.pavlenko@mail.ru>
2022-09-30 10:56:25 +0300

    partialMain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,27 +8,26 @@ import MainPage from './components/MainPage/MainPage';
 import { CHECK_AUTH_THUNK } from './redux/action/signupActions';
 import FirstPage from './components/FirstPage/FirstPage';
 
-
 export default function App({ userSession, notes }) {
   const { signup } = useSelector((state) => state);
   const State = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    fetch('http://localhost:3001/auth', {
-      credentials: 'include',
-    })
-      .then((res) => res.json())
-      .then((data) => (
-        dispatch(CHECK_AUTH_THUNK(data))
-      ));
-  }, []);
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/auth', {
+  //     credentials: 'include',
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => (
+  //       dispatch(CHECK_AUTH_THUNK(data))
+  //     ));
+  // }, []);
   return (
 
     <div className="container">
       <div className="row">
         <NavBar />
-        <FirstPage />
+        {/* <FirstPage /> */}
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/signup" element={<SignUp />} />
--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -4,17 +4,17 @@ import { ButtonGroup, Button } from 'reactstrap';
 import NavBar from '../Navbar/NavBar';
 
 export default function MainPage() {
-   return (
-      <ButtonGroup vertical size="lg">
-         <Button color="secondary">
-            New project
-         </Button>
-         <Button color="secondary">
-            Edit
-         </Button>
-         <Button color="secondary">
-            Statistics
-         </Button>
-      </ButtonGroup>
-   );
+  return (
+    <ButtonGroup vertical size="lg">
+      <Button color="secondary">
+        New project
+      </Button>
+      <Button color="secondary">
+        Edit
+      </Button>
+      <Button color="secondary">
+        Statistics
+      </Button>
+    </ButtonGroup>
+  );
 }
