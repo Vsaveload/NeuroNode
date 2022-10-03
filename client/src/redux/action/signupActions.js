ec8b3d023257b0b6b42a2db53154a2c2b7999532
keruha <kirpan1994@mail.ru>
2022-10-03 10:59:19 +0300

    redux user actions done

--- client/src/redux/action/signupActions.js
+++ client/src/redux/action/signupActions.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
   SET_SIGNUP, SET_LOGIN, LOGOUT, SET_AUTH,
 } from '../types';
@@ -6,3 +7,29 @@ export const setSignup = (data) => ({ type: SET_SIGNUP, payload: data });
 export const setLogin = (data) => ({ type: SET_LOGIN, payload: data });
 export const logout = () => ({ type: LOGOUT });
 export const CHECK_AUTH_THUNK = (data) => ({ type: SET_AUTH, payload: data });
+
+export const signupUser = (e, inputs) => (dispatch) => {
+  e.preventDefault();
+  axios.post('http://localhost:3001/auth/signup', inputs)
+    .then((res) => dispatch(setSignup(res.data)))
+    .catch(console.log);
+};
+
+export const loginUser = (e, inputs) => (dispatch) => {
+  e.preventDefault();
+  axios.post('http://localhost:3001/auth/login', inputs)
+    .then((res) => dispatch(setLogin(res.data)))
+    .catch(console.log);
+};
+
+export const logoutUser = () => (dispatch) => {
+  axios('http://localhost:3001/auth/logout')
+    .then(() => dispatch(logout()))
+    .catch(console.log);
+};
+
+export const checkAuth = () => (dispatch) => {
+  axios.post('http://localhost:3001/auth/check')
+    .then((res) => dispatch(CHECK_AUTH_THUNK(res.data)))
+    .catch(console.log);
+};
