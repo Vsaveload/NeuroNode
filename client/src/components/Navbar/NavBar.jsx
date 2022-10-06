51c8dd13248ff3acd55e36fb3fa8f139e4c99455
Mihlap <mihlap@mail.ru>
2022-10-06 15:32:17 +0300

    day8.4

--- client/src/components/Navbar/NavBar.jsx
+++ client/src/components/Navbar/NavBar.jsx
@@ -19,34 +19,34 @@ export default function Navbar() {
   };
 
   return (
-    <div className="navbar border fixed-top">
+    <div className="navbar border fixed-top" id="navbar0">
       <nav className="navbar navbar-expand-lg bg-black" id="navbar">
-        <div className="container-fluid">
-          {/* <NavLink className="navbar-brand" to="/">NeuroNode</NavLink> */}
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse " id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 d-md-flex float-sm-right nav">
+        {/* <div className="container-fluid"> */}
+        {/* <NavLink className="navbar-brand" to="/">NeuroNode</NavLink> */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 d-md-flex float-sm-right" id="nav">
               <li><NavLink className="btn btn-dark m-2" to="/">NEURO NODE</NavLink></li>
-              <div className="">
-                <li>
-                  <NavLink className="btn btn-dark m-2 float-sm-right" style={{}} to="/home">HOME</NavLink>
-                </li>
-                {!signup
-                  ? (
-                    ''
-                  ) : (
-                    <li>
-                      {/* <NavLink to="/game"
+            <div id="lid">
+              <li id="home">
+                <NavLink className="btn btn-dark m-2" style={{}} to="/home" id="navBut">HOME</NavLink>
+              </li>
+              {!signup
+                ? (
+                  ''
+                ) : (
+                  <li>
+                    {/* <NavLink to="/game"
                   className="btn btn-outline-dark m-2"><strong>Node</strong></NavLink> */}
-                      <a onClick={logoutHandler} className="btn btn-dark m-2 float-sm-right" href="logout">EXIT</a>
-                    </li>
-                  )}
-              </div>
-            </ul>
-          </div>
+                    <a onClick={logoutHandler} className="btn btn-dark m-2" href="logout" id="navBut">EXIT</a>
+                  </li>
+                )}
+            </div>
+          </ul>
         </div>
+        {/* </div> */}
       </nav>
     </div>
   );
